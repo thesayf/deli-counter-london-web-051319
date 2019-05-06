81ac55b1abae902177f868fe73af49ca1bef28c4
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-06 18:00:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(line)
+  
+  new_line = []
+  number = 1
+
+  if line.length == 0
+
+  puts "The line is currently empty."
+
+  else 
+
+   line.each do |name|
+          
+          new_line.push("#{number}.#{name}")
+          number+=1
+
+  end
+  end
+
+  return "the line is currently #{new_line}"
+  
+end
